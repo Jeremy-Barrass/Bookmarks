31323d785ce7e8df11b5013342e2cc9ee3bf0abf
Jeremy Barrass <jeremybarrass@Jeremys-MacBook-Pro.local>
2016-02-10 10:59:46 +0000

    Git re-initialize

---
+++ app/app.rb
@@ -0,0 +1,24 @@
+ENV['RACK_ENV'] ||= 'development'
+
+require 'sinatra/base'
+require_relative 'models/link'
+
+class BookmarkManager < Sinatra::Base
+  get '/' do
+    @Links = Link.all
+    p @Links[0]
+    erb :home
+  end
+
+  get '/new' do
+    "hello"
+    erb :new_links
+  end
+
+  post '/' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/'
+  end
+
+  run! if app_file == $0
+end
